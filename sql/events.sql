77dffdfde94a8db7921e1aa017dd9c4dbe6d12a3
David Timovski <davidtimovski@protonmail.com>
2022-06-27 23:08:42 +0200

    Fix formatting

--- sql/events.sql
+++ sql/events.sql
@@ -1,21 +1,21 @@
 CREATE TABLE public.events
 (
     id serial NOT NULL,
-	room_id integer NOT NULL,
-	connection_id integer NOT NULL,
-	type smallint NOT NULL,
+    room_id integer NOT NULL,
+    connection_id integer NOT NULL,
+    type smallint NOT NULL,
     occurred timestamp with time zone NOT NULL,
-	CONSTRAINT "PK_events" PRIMARY KEY (id),
+    CONSTRAINT "PK_events" PRIMARY KEY (id),
     CONSTRAINT "FK_events_rooms_room_id" FOREIGN KEY (room_id)
     REFERENCES public.rooms (id) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE CASCADE
-	DEFERRABLE INITIALLY DEFERRED,
-	CONSTRAINT "FK_events_connections_connection_id" FOREIGN KEY (connection_id)
+    DEFERRABLE INITIALLY DEFERRED,
+    CONSTRAINT "FK_events_connections_connection_id" FOREIGN KEY (connection_id)
     REFERENCES public.connections (id) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE CASCADE
-	DEFERRABLE INITIALLY DEFERRED
+    DEFERRABLE INITIALLY DEFERRED
 )
 WITH (
     OIDS = FALSE
@@ -24,4 +24,4 @@ TABLESPACE pg_default;
 
 ALTER TABLE public.rooms
     OWNER to postgres;
-	
+    
